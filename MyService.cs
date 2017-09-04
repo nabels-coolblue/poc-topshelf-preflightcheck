5cf0b63f71e582af0360330e7cd93e0c3d88af61
Niels Abels <n.abels@coolblue.nl>
2017-09-04 17:55:44 +0200

    Implements TopShelf service that has a --preflight switch.

---
+++ MyService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poc_windowsservice_preflightcheck
+{
+    public class MyService
+    {
+        public void Stop()
+        {
+            Console.WriteLine("Service is exiting.");
+        }
+
+        public void Start(bool preflight)
+        {
+            if (preflight)
+            {
+                Console.WriteLine("Running a preflight check...");
+
+                var canBeZeroOrOne = new Random().Next(2);
+
+                if (canBeZeroOrOne == 0)
+                {
+                    Console.WriteLine("Pre-flight check successful. Process is ready to run!");
+                    Console.WriteLine("Returning exit code 0.");
+                }
+                else
+                {
+                    Console.WriteLine("Pre-flight check failed.");
+                    Console.WriteLine("Returning exit code 1.");
+                }
+
+                Console.WriteLine("Type the following command on your Powershell terminal to find the exit code of the most recently exited process:");
+                Console.WriteLine("echo $LastExitCode");
+
+                Environment.Exit(canBeZeroOrOne);
+            }
+            else
+            {
+                Console.WriteLine("Pre-flight mode wasn't specified. Running normally.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Topshelf;
+
 namespace poc_windowsservice_preflightcheck
 {
     class Program
     {
-        static void Main(string[] args)
+        static bool _preflight = false;
+
+        public static void Main(string[] args)
         {
+            HostFactory.Run(x =>
+            {
+                x.Service<MyService>(s =>
+                {
+                    s.WhenStarted(service => service.Start(_preflight));
+                    s.WhenStopped(service => service.Stop());
+                    s.ConstructUsing(() => new MyService());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Service that has a pre-flight check. Run with --preflight argument to test.");
+                x.SetDisplayName("My TopShelf service");
+                x.SetServiceName("MyTopShelfService");
+
+                x.AddCommandLineSwitch("preflight", preflight => _preflight = preflight);
+            });
         }
     }
 }
