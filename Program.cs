d79703ddc5f28e177f773f23ad11fcda7f9dfbc0
Niels Abels <n.abels@coolblue.nl>
2017-09-04 17:48:22 +0200

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poc_windowsservice_preflightcheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
